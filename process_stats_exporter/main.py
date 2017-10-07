a7b101e8218c80253413c8543ebe0b8cbfc7011b
Alberto Donato <alberto.donato@gmail.com>
2017-10-07 10:58:24 +0200

    split code
    Signed-off-by: Alberto Donato <alberto.donato@gmail.com>

--- process_stats_exporter/main.py
+++ process_stats_exporter/main.py
@@ -1,121 +1,17 @@
-#!/usr/bin/env python
+# Expose a Prometheus metrics endpoint with process stats.
 
-from collections import (
-    namedtuple,
-    defaultdict)
 from itertools import chain
 
 from prometheus_aioexporter.script import PrometheusExporterScript
-from prometheus_aioexporter.metric import MetricConfig
 
 from lxstats.process import (
     Collection,
     Collector,
     CommandLineFilter)
 
-
-ProcessStat = namedtuple(
-    'ProcessStat', ['metric', 'type', 'description', 'stat'])
-
-ProcessTasksStat = namedtuple(
-    'ProcessTaskStat', ['metric', 'type', 'description'])
-
-
-class StatsCollector:
-    """Describe and collect metrics."""
-
-    @classmethod
-    def metrics(cls):
-        """Return a list of MetricConfigs."""
-        raise NotImplementedError('Subclasses must implement metrics()')
-
-    @classmethod
-    def collect(cls, process):
-        """Return a dict mapping metric names to values for the process."""
-        raise NotImplementedError('Subclasses must implement collect()')
-
-
-class ProcessStatsCollector(StatsCollector):
-    """Collect metrics for a process."""
-
-    _STATS = (
-        ProcessStat(
-            'process_time_user', 'counter', 'Time scheduled in user mode',
-            'stat.utime'),
-        ProcessStat(
-            'process_time_system', 'counter',
-            'Time scheduled in kernel mode', 'stat.stime'),
-        ProcessStat(
-            'process_mem_rss', 'gauge', 'Memory resident segment size (RSS)',
-            'stat.rss'),
-        ProcessStat(
-            'process_maj_fault', 'counter',
-            'Number of major faults that required a page load', 'stat.majflt'),
-        ProcessStat(
-            'process_min_fault', 'counter',
-            'Number of minor faults the not required a page load',
-            'stat.minflt'),
-        ProcessStat(
-            'process_ctx_involuntary', 'counter',
-            'Number of involuntary context switches',
-            'sched.nr_involuntary_switches'),
-        ProcessStat(
-            'process_ctx_voluntary', 'counter',
-            'Number of voluntary context switches',
-            'sched.nr_voluntary_switches'),
-        ProcessStat(
-            'process_mem_rss_max', 'counter',
-            'Maximum memory resident segment size (RSS)', 'status.VmHWM'))
-
-    @classmethod
-    def metrics(cls):
-        return [
-            MetricConfig(
-                stat.metric, stat.description, stat.type, {'labels': ['cmd']})
-            for stat in cls._STATS]
-
-    @classmethod
-    def collect(cls, process):
-        process.collect_stats()
-        return {stat.metric: process.get(stat.stat) for stat in cls._STATS}
-
-
-class ProcessTasksStatsCollector(StatsCollector):
-    """Collect metrics for a process' tasks."""
-
-    _STATS = (
-        ProcessTasksStat(
-            'process_tasks_count', 'gauge', 'Number of process tasks'),
-        ProcessTasksStat(
-            'process_tasks_state_running', 'gauge',
-            'Number of process tasks in running state'),
-        ProcessTasksStat(
-            'process_tasks_state_sleeping', 'gauge',
-            'Number of process tasks in sleeping state'),
-        ProcessTasksStat(
-            'process_tasks_state_uninterruptible_sleep', 'gauge',
-            'Number of process tasks in uninterruptible sleep state'),
-    )
-
-    @classmethod
-    def metrics(cls):
-        return [
-            MetricConfig(
-                stat.metric, stat.description, stat.type, {'labels': ['cmd']})
-            for stat in cls._STATS]
-
-    @classmethod
-    def collect(cls, process):
-        tasks = process.tasks()
-        state_counts = defaultdict(int)
-        for task in tasks:
-            task.collect_stats()
-            state_counts[task.get('stat.state')] += 1
-        return {
-            'process_tasks_count': len(tasks),
-            'process_tasks_state_running': state_counts['R'],
-            'process_tasks_state_sleeping': state_counts['S'],
-            'process_tasks_state_uninterruptible_sleep': state_counts['D']}
+from .stats import (
+    ProcessStatsCollector,
+    ProcessTasksStatsCollector)
 
 
 class ProcessStatsExporter(PrometheusExporterScript):
@@ -139,7 +35,8 @@ class ProcessStatsExporter(PrometheusExporterScript):
         self._name_regexps = args.name_regexps
         if self._pids:
             self.logger.info(
-                'tracking stats for PIDs [{}]'.format(', '.join(self._pids)))
+                'tracking stats for PIDs [{}]'.format(
+                    ', '.join(str(pid) for pid in self._pids)))
         elif self._name_regexps:
             self.logger.info(
                 'tracking stats for processes [{}]'.format(
@@ -152,6 +49,7 @@ class ProcessStatsExporter(PrometheusExporterScript):
         self._metrics = self.create_metrics(metric_configs)
 
     async def on_application_startup(self, application):
+        # setup handler to update metrics on requests
         application.set_metric_update_handler(self._update_metrics)
 
     def _update_metrics(self, metrics):
---
+++ process_stats_exporter/stats.py
@@ -0,0 +1,111 @@
+# Collect metrics for processes and tasks
+
+from collections import (
+    namedtuple,
+    defaultdict)
+
+from prometheus_aioexporter.metric import MetricConfig
+
+
+ProcessStat = namedtuple(
+    'ProcessStat', ['metric', 'type', 'description', 'stat'])
+
+ProcessTasksStat = namedtuple(
+    'ProcessTaskStat', ['metric', 'type', 'description'])
+
+
+class StatsCollector:
+    """Describe and collect metrics."""
+
+    @classmethod
+    def metrics(cls):
+        """Return a list of MetricConfigs."""
+        raise NotImplementedError('Subclasses must implement metrics()')
+
+    @classmethod
+    def collect(cls, process):
+        """Return a dict mapping metric names to values for the process."""
+        raise NotImplementedError('Subclasses must implement collect()')
+
+
+class ProcessStatsCollector(StatsCollector):
+    """Collect metrics for a process."""
+
+    _STATS = (
+        ProcessStat(
+            'process_time_user', 'counter', 'Time scheduled in user mode',
+            'stat.utime'),
+        ProcessStat(
+            'process_time_system', 'counter',
+            'Time scheduled in kernel mode', 'stat.stime'),
+        ProcessStat(
+            'process_mem_rss', 'gauge', 'Memory resident segment size (RSS)',
+            'stat.rss'),
+        ProcessStat(
+            'process_maj_fault', 'counter',
+            'Number of major faults that required a page load', 'stat.majflt'),
+        ProcessStat(
+            'process_min_fault', 'counter',
+            'Number of minor faults the not required a page load',
+            'stat.minflt'),
+        ProcessStat(
+            'process_ctx_involuntary', 'counter',
+            'Number of involuntary context switches',
+            'sched.nr_involuntary_switches'),
+        ProcessStat(
+            'process_ctx_voluntary', 'counter',
+            'Number of voluntary context switches',
+            'sched.nr_voluntary_switches'),
+        ProcessStat(
+            'process_mem_rss_max', 'counter',
+            'Maximum memory resident segment size (RSS)', 'status.VmHWM'))
+
+    @classmethod
+    def metrics(cls):
+        return [
+            MetricConfig(
+                stat.metric, stat.description, stat.type, {'labels': ['cmd']})
+            for stat in cls._STATS]
+
+    @classmethod
+    def collect(cls, process):
+        process.collect_stats()
+        return {stat.metric: process.get(stat.stat) for stat in cls._STATS}
+
+
+class ProcessTasksStatsCollector(StatsCollector):
+    """Collect metrics for a process' tasks."""
+
+    _STATS = (
+        ProcessTasksStat(
+            'process_tasks_count', 'gauge', 'Number of process tasks'),
+        ProcessTasksStat(
+            'process_tasks_state_running', 'gauge',
+            'Number of process tasks in running state'),
+        ProcessTasksStat(
+            'process_tasks_state_sleeping', 'gauge',
+            'Number of process tasks in sleeping state'),
+        ProcessTasksStat(
+            'process_tasks_state_uninterruptible_sleep', 'gauge',
+            'Number of process tasks in uninterruptible sleep state'),
+    )
+
+    @classmethod
+    def metrics(cls):
+        return [
+            MetricConfig(
+                stat.metric, stat.description, stat.type, {'labels': ['cmd']})
+            for stat in cls._STATS]
+
+    @classmethod
+    def collect(cls, process):
+        tasks = process.tasks()
+        state_counts = defaultdict(int)
+        for task in tasks:
+            task.collect_stats()
+            state_counts[task.get('stat.state')] += 1
+        return {
+            'process_tasks_count': len(tasks),
+            'process_tasks_state_running': state_counts['R'],
+            'process_tasks_state_sleeping': state_counts['S'],
+            'process_tasks_state_uninterruptible_sleep': state_counts['D']}
